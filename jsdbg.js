00b11b985dec0beaa9ccf8520b6b140a4630b869
zhibirc <y.surilov@elama.ru>
2015-01-24 23:54:35 +0300

    JSDoc, naming, concept.

--- jsdbg.js
+++ jsdbg.js
@@ -1,40 +1,48 @@
-(function (root, undefined) {
+/**
+ * JSDBG, just another console wrapper with detailed tutorial and the same usage as the native one.
+ * But in addition to habitual behaviour and syntax it has some useful and even powerful possibilities.
+ * All of the them are describe in built-in documentation, so you can type ___.HELP in any time and read it.
+ *
+ * @author zhibirc
+ * @param {object} root - Global object.
+ * @returns {undefined} IIFE instantiate a new property in the global object and returns nothing.
+ */
+(function (root) {
     'use strict';
 
-    /** Available names for module name. */
-    var fit_names = ['DBG', 'DEBUG', 'DEBUGGER'],
+    /** Here we are. */
+    var con = root.console,
         has_own = Object.prototype.hasOwnProperty,
-        con = root.console,
         is_console_defined = typeof con !== 'undefined',
         base_control_flags = {
             activity: 1,
             logging: 0,
             remote: 0
         },
-        dbg_control,
+        ___reference,
         fit_props,
         support;
 
-    fit_names.some(function (elem) {
-        if (root[elem] === undefined) {
-            Object.defineProperty(root, elem, {
-                value: {}
-            });
-            dbg_control = root[elem];
-            dbg_control.NAME = elem;
-            return true;
-        }
-    });
+    if (typeof root.___ === 'undefined') {
+        Object.defineProperty(root, '___', {
+            value: {}
+        });
+        ___reference = root.___;
+    }
 
-    // TODO: this is a stand-alone part of program, documentation and nothing else. Make it easy to understand and complete for effective usage.
+    ___reference.NAME = '___';
+
+    /**
+     * @todo This is a stand-alone part of the program, documentation and nothing else. Make it easy to understand and complete for effective usage.
+     */
     support = {
         errors: {
-            wrong_flag: 'Flags may be 1 or 0 only, any other values are wrong! For details please refer to the documentation (' + dbg_control.NAME + '.HELP).'
+            wrong_flag: 'Flags may be 1 or 0 only, any other values are wrong! For details please refer to the documentation (' + ___reference.NAME + '.HELP).'
         },
         intro: {
-            0: '\u25C0 Introduction to JSDBG \u25B6\n\nNow your debug tool is available under the "' + dbg_control.NAME,
+            0: '\u25C0 Introduction to JSDBG \u25B6\n\nNow your debug tool is available under the "' + ___reference.NAME,
             1: '" name. Naturally, as the built-in console it has a lot of methods and some properties, full list of which and information about common usage are always available by typing\n\n',
-            2: '> ' + dbg_control.NAME + '.HELP',
+            2: '> ' + ___reference.NAME + '.HELP',
             3: '\n\nInformation about methods is identical to standard native console ones. But keep in mind that there are additional functionality and possibilities, so read documentation.'
         },
         state: {
@@ -59,7 +67,7 @@
         for (var method in con) {
             (function (m) {
                 if (con[m] && ~fit_props.indexOf(m)) {
-                    Object.defineProperty(dbg_control, m, {
+                    Object.defineProperty(___reference, m, {
                         value: function () {
                             if (base_control_flags.activity === 1) {
                                 con[m].apply(con, arguments);
@@ -73,7 +81,7 @@
         }
     }
 
-    dbg_control.setState = function (activity, logging, remote) {
+    ___reference.setState = function (activity, logging, remote) {
         if (activity === 1) {
             base_control_flags.activity = 1;
         } else if (activity === 0) {
@@ -83,9 +91,9 @@
         }
     };
 
-    dbg_control.VERSION = '1.0';
-    dbg_control.HELP = support.help;
-    dbg_control.STATE = support.state.header + Object.keys(support.state.body).reduce(function (init, el) { return init += support.state.body[el]; }, '');
+    ___reference.VERSION = '1.0';
+    ___reference.HELP = support.help;
+    ___reference.STATE = support.state.header + Object.keys(support.state.body).reduce(function (init, el) { return init += support.state.body[el]; }, '');
 
-    Object.freeze(dbg_control);
+    Object.freeze(___reference);
 }(window));
